118f77af67dc30f87096e2221bea19d672b16070
huynhnhutran114 <33045161+huynhnhutran114@users.noreply.github.com>
2020-07-05 08:18:28 +0700

    Update qlbanhang.sql

--- qlbanhang.sql
+++ qlbanhang.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th6 14, 2019 lúc 06:38 PM
--- Phiên bản máy phục vụ: 10.1.36-MariaDB
--- Phiên bản PHP: 7.2.11
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
